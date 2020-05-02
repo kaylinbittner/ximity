5106c936d5b3305e9fcf1adb42c1389fce5d03fa
Kaylin Bittner <kaylinbittner@gmail.com>
2020-05-02 08:51:54 -0400

    routes completed for offers and show page links working and index method written

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,35 @@
 class OffersController < ApplicationController
+    # Every route needs a corresponding method in the controller and then a matching named view template
     def index
         @offers = Offer.all
     end
+
+    def show
+        @offer = Offer.find(params[:id])
+    end
+
+    def create
+        @offer = Offer.new(offer_params)
+        if @offer.save
+          redirect_to offer_path(@offer)
+        else
+          render :new
+        end
+      end
+    
+      def destroy
+        @offer = Offer.find(params[:id])
+        if @offer.destroy
+          redirect_to offers_path
+        else
+          render 'offers/show'
+        end
+      end
+    
+      private
+    
+      def offer_params
+        params.require(:offer).permit(:title, :description, :picture)
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  resources :offers
+
+  # Megan - `resources :offers` generates the 7 CRUD routes for the offers controller
+  # I wrote them out manually below so you can see how that would be done ☺
+  # resources :offers
+
+  get    "offers",          to: "offers#index"
+
+  get    "offers/new",      to: "offers#new",  as: :new_offer
+  post   "offers",          to: "offers#create"
+
+  # IMPORTANT: The `show` route needs to be *after* `new` route or else you will recieve a routing error
+  # Honestly, I cannot remember why this is so, but it is lol
+  get    "offers/:id",      to: "offers#show", as: :offer
+
+  get    "offers/:id/edit", to: "offers#edit", as: :edit_offer
+  patch  "offers/:id",      to: "offers#update"
+
+  delete "offers/:id",      to: "offers#destroy"
 end
